c44c1348ee5f6f10a4b5dba3f143eab3597c3149
RodrosD <57403194+RodrosD@users.noreply.github.com>
2020-01-28 16:33:03 +0100

    Add files via upload

---
+++ classLibro.py
@@ -0,0 +1,10 @@
+class Libro:
+
+    # Constructor
+    def __init__(self, titulo, autor, editorial, isbn):
+        self.titulo = titulo
+        self.autor = autor
+        self.editorial = editorial
+        self.isbn = isbn
+    
+
