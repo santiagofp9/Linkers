a2e1b50be68e7bb9214ed33001baf5edbc3d2cd1
Victor-EA <57403468+Victor-EA@users.noreply.github.com>
2020-02-06 16:42:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from biblioteca import Biblioteca
 
-menu1=Biblioteca()
-menu1.menu_inicial()
+menu=Biblioteca()
+menu.menu_inicial()
 opcion=input("\nIngrese opción: ")
-menu1.iniciar(opcion)
+menu.iniciar(opcion)
